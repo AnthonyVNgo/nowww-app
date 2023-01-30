1e49481004ebbf1787e9acb0a5f480c0756eed29
Anthony Ngo <Anthony.v.ngo@gmail.com>
2023-01-29 16:26:45 -0800

    chore: fix update now entry

--- client/src/components/now-input-form.jsx
+++ client/src/components/now-input-form.jsx
@@ -131,13 +131,17 @@ const NowInputForm = (props) => {
 
             {!props.isLi && 
               <div className="pt-1">
-                <button disabled={inputIsDisabled} type="submit" className="btn btn-primary w-100">+</button>
+                <button disabled={inputIsDisabled} type="submit" className="btn btn-primary w-100">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                  </svg>
+                </button>
               </div>
             }
 
             {props.isLi && 
             <div>
-              <button className="btn w-100" onClick={handleDeleteButton}>
+              <button className="btn w-100" onClick={handleDeleteButton} type="button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
                   <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
                 </svg>
